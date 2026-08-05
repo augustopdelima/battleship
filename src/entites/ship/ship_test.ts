7014abba4c1f20ccfcf0b27584c6a400f87f09db
Augusto Pasquali <augusto.pasqualidelima@gmail.com>
2026-08-05 15:45:46 -0300

    test(ship): cover ship creation, hits and sinking

---
+++ src/entites/ship/ship_test.ts
@@ -0,0 +1,86 @@
+import { assertEquals } from "@std/assert";
+import { createShip, Ship } from "./ship.ts";
+import { assertNotEquals } from "@std/assert/not-equals";
+
+const ships = [
+  ["patrolBoat", 2],
+  ["submarine", 3],
+  ["destroyer", 3],
+  ["battleship", 4],
+  ["aircraftCarrier", 5],
+] as const;
+
+const hitManyTimes = (hits: number, ship: Ship): Ship =>
+  hits === 0 ? ship : hitManyTimes(hits - 1, ship.hit());
+
+Deno.test("Should create a battleship with length 4", () => {
+  const ship = createShip("battleship");
+
+  assertEquals(ship.getLength(), 4);
+});
+
+Deno.test("Should sink after receiving enough hits", () => {
+  const ship = createShip("destroyer");
+
+  const newship = hitManyTimes(3, ship);
+
+  assertEquals(newship.isSunk(), true);
+});
+
+Deno.test("Should create a battleship", () => {
+  const ship = createShip("battleship");
+
+  assertEquals(ship.getType(), "battleship");
+});
+
+Deno.test("Should not be sunk when created", () => {
+  const ship = createShip("battleship");
+  assertEquals(ship.isSunk(), false);
+});
+
+Deno.test("Should not be sunk after one hit", () => {
+  const ship = createShip("battleship");
+
+  const damagedShip = ship.hit();
+  assertEquals(damagedShip.isSunk(), false);
+});
+
+Deno.test("Should not sink before receiving enough hits", () => {
+  const ship = createShip("battleship");
+
+  const damagedShip = hitManyTimes(3, ship);
+
+  assertEquals(damagedShip.isSunk(), false);
+});
+
+Deno.test("Should not mutate the original ship when hit", () => {
+  const ship = createShip("battleship");
+
+  const damagedShip = ship.hit();
+
+  assertEquals(ship.getHits(), 0);
+  assertEquals(damagedShip.getHits(), 1);
+});
+
+Deno.test("Should return a new ship after being hit", () => {
+  const ship = createShip("battleship");
+
+  const damagedShip = ship.hit();
+
+  assertNotEquals(ship, damagedShip);
+});
+
+Deno.test("Should not register hits after the ship is sunk", () => {
+  const ship = createShip("patrolBoat");
+
+  const damagedShip = hitManyTimes(5, ship);
+  assertEquals(damagedShip.getHits(), 2);
+});
+
+ships.forEach(([type, expectedLength]) => {
+  Deno.test(`Should create a ${type} with length ${expectedLength}`, () => {
+    const ship = createShip(type);
+
+    assertEquals(ship.getLength(), expectedLength);
+  });
+});
